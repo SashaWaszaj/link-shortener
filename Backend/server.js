c2ad32d18eabd969c77d045a14666c57eb4196fb
Sasha Waszaj <swaszajf@gmail.com>
2025-01-28 16:30:38 -0300

    server updated

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,7 @@ connectDB();
 app.use(cors());
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5173", "https://url-shortener-service-app.netlify.app/",
     methods: ["GET", "POST"],
 }));
 
