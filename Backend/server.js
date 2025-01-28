adc822890bfd677df5b06ba4f314714714b44e3c
Sasha Waszaj <swaszajf@gmail.com>
2025-01-28 16:51:04 -0300

    server updated

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const connectDB = require('./config/db');
 const cors = require("cors");
-const config = require('./config');
+const config = require('./config/config');
 require('dotenv').config();
 
 const app = express();
