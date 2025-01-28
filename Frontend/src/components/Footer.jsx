f642a55d2f5a3487050da1ec6ce43c7c7ece428f
Sasha Waszaj <swaszajf@gmail.com>
2025-01-28 16:26:07 -0300

    img uploaded

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -1,21 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import UrlShortenerGray from "../img/urlShortenerGray.png";
+import UrlShortenerGray2 from "../assets/urlShortenerGray2.PNG";
+import UrlShortenerWhite2 from "../assets/urlShortenerWhite2.PNG";
 import "../styles/Footer.css";
 
 
 const NavBar = () => {
+        const [theme, setTheme] = useState(document.body.getAttribute("data-theme"));
+          const [logoSrc, setLogoSrc] = useState(
+            theme === "dark" ? UrlShortenerGray2 : UrlShortenerWhite2
+          );
+        
+          useEffect(() => {
+            // Observador para detectar cambios en el atributo data-theme del body
+            const observer = new MutationObserver(() => {
+              const currentTheme = document.body.getAttribute("data-theme");
+              setTheme(currentTheme); // Actualiza el estado local
+            });
+        
+            observer.observe(document.body, { attributes: true, attributeFilter: ["data-theme"] });
+        
+            return () => observer.disconnect(); // Limpieza al desmontar el componente
+          }, []);
+        
+          useEffect(() => {
+            // Actualiza el logo cada vez que el tema cambia
+            const newLogo = theme === "dark" ? UrlShortenerGray2 : UrlShortenerWhite2;
+            setLogoSrc(newLogo);
+          }, [theme]);
+    
+
 
     return(
             <footer className="footer">
                 <div className="footer-container">
                     <div>
                         <Link to={ "/"}>
-                            <img
-                                src={ UrlShortenerGray }
-                                alt="URL Shortener Logo"
-                                className="logo-img-gray-footer"
-                            />
+                        <img src={logoSrc} alt="URL Shortener Logo" className="logo-img-gray" />
                         </Link>
                     </div>
                     <div className="footer-bottom">
