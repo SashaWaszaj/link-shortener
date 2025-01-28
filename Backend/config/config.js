21487cfc88148f50d2bc02939f424901249b2cd1
Sasha Waszaj <swaszajf@gmail.com>
2025-01-28 17:22:30 -0300

    server updated

--- Backend/config/config.js
+++ Backend/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: "mongodb+srv://Sasha:Waszajsasha1@sashawf.j7d1wjx.mongodb.net/",
+    MONGO_URI: "mongodb+srv://Sasha:Waszajsasha1@sashawf.j7d1wjx.mongodb.net/",
     baseUrl: process.env.NODE_ENV === 'production' 
        ? 'BASE_URL=https://link-shortener-ansq.onrender.com'
        : 'http://localhost:8080' 
