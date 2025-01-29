2928168f571d5b1c8c056da6a581b1562a70f2b9
Sasha Waszaj <swaszajf@gmail.com>
2025-01-28 21:13:40 -0300

    server updated

--- Backend/server.js
+++ Backend/server.js
@@ -6,17 +6,31 @@ require('dotenv').config();
 
 const app = express();
 
-// Configuración de CORS
+// Lista de dominios permitidos
 const allowedOrigins = [
-    'https://url-shortener-service-app.netlify.app', 
-    'http://localhost:5173', 
+    'https://url-shortener-service-app.netlify.app/', // Frontend en producción
+    'http://localhost:5173', // Frontend en desarrollo
 ];
 
+// Configuración de CORS
 app.use(cors({
-    origin: allowedOrigins, 
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['GET', 'POST'], // Métodos HTTP permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
 }));
+
+app.use((err, req, res, next) => {
+    if (err instanceof Error && err.message === 'Not allowed by CORS') {
+        return res.status(403).json({ error: 'CORS policy does not allow access from this origin.' });
+    }
+    next(err);
+});
 
 app.use(express.json({ extended: false }));
 
